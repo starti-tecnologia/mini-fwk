435dbdb8f93632e1e77b2490cec9560bf1486b8d
Jeferson Daniel <jeferson.daniel@startione.mobi>
2016-02-29 16:18:41 -0300

    Fixing QueryAware

--- src/Entity/Behaviors/QueryAware.php
+++ src/Entity/Behaviors/QueryAware.php
@@ -37,8 +37,7 @@ trait QueryAware
      */
     public static function instance()
     {
-        $class = self::class;
-        $obj = new $class;
+        $obj = new self;
         self::$instanceTable = $obj->table;
         if (isset($obj->idAttribute)) self::$instanceIdAttribute = $obj->idAttribute;
         self::$instanceUseSoftDeletes = $obj->useSoftDeletes;
